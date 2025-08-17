37625d5ee5e1f2c22137153fb796fc9e8b5d0729
Darkbeast-glitch <bbjulius4@gmail.com>
2025-08-17 14:16:15 +0000

    Remove default localhost value for database host in production settings

--- djangoCampus/settings/settings_prod.py
+++ djangoCampus/settings/settings_prod.py
@@ -37,7 +37,7 @@ DATABASES = {
         "NAME": os.environ.get("DB_NAME"),
         "USER": os.environ.get("DB_USER"),
         "PASSWORD": os.environ.get("DB_PASSWORD"),
-        "HOST": os.environ.get("DB_HOST", "localhost"),
+        "HOST": os.environ.get("DB_HOST"),
         "PORT": os.environ.get("DB_PORT", "3306"),
         "OPTIONS": {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
